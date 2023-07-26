d2c4064b3223c94066b216a8f8122a473f01c7b2
robert <r.gabriel.f@outlook.com>
2023-07-26 10:59:14 -0400

    clientes realizado

--- src/conponents/Servicios.js
+++ src/conponents/Servicios.js
@@ -1,117 +1,125 @@
 import { React, useState, useEffect } from "react";
-import nn from "../assets/img/ojo.png";
+import n from "../assets/img/ojo.png";
 import "./Servicios.css";
 import azucarera from "../assets/img/servicios/azucarera.jpg";
 import cementera from "../assets/img/servicios/cementera.jpg";
 import mineria from "../assets/img/servicios/mineria.jpg";
 import petrolera from "../assets/img/servicios/petrolera.jpg";
-
-import acerodecarbono from "../assets/img/fabricamos/acerodecarbono.jpg";
-import aceroinoxidable from "../assets/img/fabricamos/acerosinoxidables.jpg";
-import broncedealuminio from "../assets/img/fabricamos/broncealuminio.jpg";
-import broncefosforoso from "../assets/img/fabricamos/broncefosforoso.jpg";
-import fierrofundido from "../assets/img/fabricamos/fierrofundido.jpg";
+import construccion from "../assets/img/servicios/construccion.png";
+import general from "../assets/img/servicios/general.png";
 
 export const Servicios = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [currentImageFrabricamosIndex, setCurrentImageFabricamosIndex] =
-    useState(0);
-  const images = [azucarera, cementera, mineria, petrolera];
-  const imagesFrabricamos = [
-    fierrofundido,
-    broncedealuminio,
-    broncefosforoso,
-    acerodecarbono,
-    aceroinoxidable,
-  ];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  useEffect(() => {
-    const Interval = setInterval(() => {
-      setCurrentImageFabricamosIndex(
-        (prevIndex) => (prevIndex + 1) % imagesFrabricamos.length
-      );
-    }, 5000);
-    return () => {
-      clearInterval(Interval);
-    };
-  }, []);
-
   return (
     <section className="container-servicios">
-      <div className="contenido">
-        <div className="servicios">
-          <div className="nombre-servicios">
-            <h2>Clientes</h2>
-          </div>
-          <div className="container-img-servicios">
-            <div className="img-servicios">
-              <img
-                src={images[currentImageIndex]}
-                alt="ser"
-                className="img-servicio"
-              />
-            </div>
-            <div className="descripcion-servicios">
-              <p>TECNOACERO S.R.L.</p>
-              <p>
-                Actualmete atiende a clientes a nivel nacional de los siguientes
-                segmentos:
-              </p>
-              <ul>
-                <li>Industria Minera</li>
-                <li>Industria Cementera</li>
-                <li>Industria Petrolera</li>
-                <li>Industria Azucarera</li>
-                <li>Industria en General</li>
-              </ul>
-            </div>
+      <div className="nombre-servicios">
+        <h2>Clientes</h2>
+        <h2>Tecnoacero S.R.L.</h2>
+      </div>
+      <div className="container-img-servicios">
+        <div className="img-servicio">
+          <img src={mineria} href className="img-servicios" />
+          <div className="tipo-servicio">
+            <h3>Industrias Minera</h3>
           </div>
         </div>
-        <div className="servicios">
-          <div className="nombre-servicios">
-            <h2>Fabricamos acero y sus aleaciones</h2>
+        <div className="img-servicio">
+          <img src={cementera} href className="img-servicios" />
+          <div className="tipo-servicio">
+            <h3>Industrias Cementera</h3>
           </div>
-          <div className="container-img-servicios">
-            <div className="descripcion-servicios">
-              <ol>
-                <li>Acero al Carbono</li>
-                <ul>
-                  <li>Bajo Carbono</li>
-                  <li>Medio Carbono</li>
-                  <li>Alto Carbono</li>
-                  <li>Alto Carbono al Cromo Moly</li>
-                  <li>Aceros Aleados Especiales</li>
-                </ul>
-                <li>Fierro Nodular</li>
-                <li>Fierro Fundido</li>
-                <li>Hierro ni Hard</li>
-                <li>Aceros Inoxidables</li>
-                <li>Kromox</li>
-                <li>Acero al Manganeso</li>
-                <li>Bronce Fosforoso</li>
-                <li>Bronce al Aluminio</li>
-              </ol>
-            </div>
-            <div className="img-servicios">
-              <img
-                src={imagesFrabricamos[currentImageFrabricamosIndex]}
-                alt="ser"
-                className="img-servicio"
-              />
-            </div>
+        </div>
+        <div className="img-servicio">
+          <img src={petrolera} href className="img-servicios" />
+          <div className="tipo-servicio">
+            <h3>Industrias Petrolera</h3>
+          </div>
+        </div>
+        <div className="img-servicio">
+          <img src={azucarera} href className="img-servicios" />
+          <div className="tipo-servicio">
+            <h3>Industrias Azucarera</h3>
+          </div>
+        </div>
+        <div className="img-servicio">
+          <img src={construccion} href className="img-servicios" />
+          <div className="tipo-servicio">
+            <h3>Industrias de Construcción</h3>
+          </div>
+        </div>
+        <div className="img-servicio">
+          <img src={general} href className="img-servicios" />
+          <div className="tipo-servicio">
+            <h3>Industrias en General</h3>
           </div>
         </div>
       </div>
+      <div className="piezas-servicio-nombre">
+        <h2>Fabricamos acero y sus aleaciones</h2>
+      </div>
+      <div className="container-parts-servicio">
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Acero al Carbono</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Fierro Nodular</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Fierro Fundido</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Hierro ni Hard</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Aceros Inoxidables</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Kromox</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Acero al Manganeso</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Bronce Fosforoso</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Bronce al Aluminio</h3>
+          </div>
+        </div>
+        <div className="piezas-servicio">
+          <img src={n} className="img-pieza-servicio" />
+          <div className="nombre-pieza-servicio">
+            <h3>Aceros Aleados Especiales</h3>
+          </div>
+        </div>
+      </div>
+
+      <div></div>
     </section>
   );
 };

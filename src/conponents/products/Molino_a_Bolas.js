53477d4214a5439620a70eee2b5f77c0962a0c00
robert <r.gabriel.f@outlook.com>
2023-08-12 13:12:52 -0400

    realizamos molino repuestos

--- src/conponents/products/Molino_a_Bolas.js
+++ src/conponents/products/Molino_a_Bolas.js
@@ -10,6 +10,7 @@ import polea from "../../assets/img/molino_a_bola/polea.jpg";
 import tapa from "../../assets/img/molino_a_bola/tapa.jpg";
 import trunion from "../../assets/img/molino_a_bola/TRUNION.jpg";
 import trunionretocado from "../../assets/img/molino_a_bola/TRUNION RETOCADO.jpg";
+import molino from "../../assets/img/molino a bolas.png";
 export const Molino_a_Bolas = () => {
   return (
     <section className="container-productosestilos" transition-style>
@@ -117,17 +118,139 @@ export const Molino_a_Bolas = () => {
           </div>
         </div>
       </div>
+
+      <div className="image-partes">
+        <img src={molino} alt="molino" className="imagen-partes" />
+      </div>
+
       <div className="container-descripcion-partes">
-        <h3>Descripcion</h3>
-        <p>
-          Superar las expectativas de nuestros clientes con satisfacer las
-          necesidades y los requerimientos de las industrias mediante la
-          excelencia y necesidades y los requerimientos de las industrias
-          mediante la excelencia y calidad de los productos que elaboramos,
-          trabajar con entrega y prefesionalismo para ofrecer la mejor calidad,
-          beneficiando a nuestros clientes con la adquisición de sus pedidos
-          (acero)
-        </p>
+        <h3>Caracteristicas</h3>
+        <table className="caracteristicas-table-partes">
+          <thead>
+            <tr>
+              <th>ITEM</th>
+              <th>CTD</th>
+              <th>DETALLE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>
+                Tapas de Molino en acero SAE 1045. Con perforaciones para
+                solera, cuello, anillo y catalina.
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>1</td>
+              <td>
+                Trunion de entrada, hierro fundido gris ASTM A48 – Grado 25.
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>1</td>
+              <td>
+                Trunion de salida, hierro fundido gris ASTM A48 – Grado 25.
+              </td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>2</td>
+              <td>Cuello de molino de acero SAE 1045.</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>1</td>
+              <td>Catalina en acero SAE 1045.</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>1</td>
+              <td>Piñon en acero SAE 1045.</td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>1</td>
+              <td>
+                Eje de piñon en acero SAE 1045. Tres ajustes para rodamiento uno
+                para polea, uno para pinos.
+              </td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>1</td>
+              <td>Polea de transmisión de hierro fundido gris.</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>2</td>
+              <td>Anillas en acero SAE 1020.</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>3 JUEGOS</td>
+              <td>
+                Tres apoyos (descansos), tres bases de eje de piñon y polea
+                transmitida. Material: hierro fundido gris.
+              </td>
+            </tr>
+            <tr>
+              <td>11</td>
+              <td>2 JUEGOS</td>
+              <td>
+                Juego consistente de base, descanso y tapa de descanso para
+                cuello de molino. Superficie de contacto recubierto con metal
+                blanco.
+              </td>
+            </tr>
+            <tr>
+              <td>12</td>
+              <td>1</td>
+              <td>
+                Cilindro de planchas SEA 1010. Espesor según requerimiento.
+              </td>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>1 JUEGO</td>
+              <td>
+                Un juego de soleras para el cilindro. Material: acero de cromo
+                molibdeno.
+              </td>
+            </tr>
+            <tr>
+              <td>14</td>
+              <td>1 JUEGO</td>
+              <td>
+                Un juego de soleras para tapa. Material: acero cromo cromo
+                molibdeno.
+              </td>
+            </tr>
+            <tr>
+              <td>15</td>
+              <td>1 juego</td>
+              <td>Pernos de sujeción para soleras.</td>
+            </tr>
+            <tr>
+              <td>16</td>
+              <td>3 Pz</td>
+              <td>
+                Rodamiento de rodillos a rotula, según requerimiento de tamaño
+                de molinos.
+              </td>
+            </tr>
+            <tr>
+              <td>17</td>
+              <td>3 Pz</td>
+              <td>
+                Mango y tuerca de fijación para rodamiento, según requerimiento.
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
   );
